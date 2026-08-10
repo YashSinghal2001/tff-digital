dd9d6e20f209e1ab1349f5b7dfd420974e086028
YashSinghal2001 <ysinghal30@gmail.com>
2026-08-10 15:48:27 +0530

    updating links in footer

---
+++ src/constants/social.ts
@@ -0,0 +1,7 @@
+export const SOCIAL_LINKS = {
+  x: "https://x.com/tffdigital",
+  instagram: "https://www.instagram.com/tffdigital/",
+  linkedin: "https://www.linkedin.com/company/tff-digital/",
+  facebook: "https://www.facebook.com/profile.php?id=61592027200334",
+  pinterest: "https://www.pinterest.com/tffdigital/",
+} as const;
